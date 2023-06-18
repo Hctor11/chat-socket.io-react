66f6f02937966a840efd3ca61fa88524f28be876
hector <hectorrrivera1010@gmail.com>
2023-06-17 18:21:38 -0600

    added fontend module

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from 'express'
-import { Server as SocketServer } from 'socket.io';
+import http from 'http'
+import { Server } from 'socket.io';
 
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
-app.listen(3000)
+server.listen(3000)
 console.log('Server on port ', 3000);
