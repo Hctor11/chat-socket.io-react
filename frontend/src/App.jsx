3efe893c4c0f023f600a9e23ef0684c5d2a72732
hector <hectorrrivera1010@gmail.com>
2023-06-19 00:44:11 -0600

    added message optimization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,54 +1,49 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import io from 'socket.io-client'
+import { useState, useEffect } from "react";
+import "./App.css";
+import io from "socket.io-client";
 
 // establecemos la conexion con el servidor
-const socket = io("/")
+const socket = io("/");
 
 function App() {
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
 
   const handleSubmit = (e) => {
-      e.preventDefault();
-      setMessages([...messages, message]);
-      socket.emit('message', message);
-  }
+    e.preventDefault();
+    setMessages([...messages, message]);
+    socket.emit("message", message);
+  };
 
   useEffect(() => {
-    socket.on('message', message => {
-      console.log(message);
-      receiveMessage(message);
-    })
-  }, [])
+    socket.on("message", receiveMessage);
 
-  const receiveMessage = message => setMessages(state => [...state, message])
+    return () => {
+      socket.off("message", receiveMessage);
+    };
+  }, []);
+
+  const receiveMessage = (message) =>
+    setMessages((state) => [...state, message]);
 
   return (
     <div className="container">
+      <ul>
+        {messages.map((message, i) => (
+          <li key={i}>{message}</li>
+        ))}
+      </ul>
 
       <form onSubmit={handleSubmit}>
-        <input 
-          type="text" 
-          placeholder='Ingrese su mensaje...'
+        <input
+          type="text"
+          placeholder="Ingrese su mensaje..."
           onChange={(e) => setMessage(e.target.value)}
         />
-        <button>
-          Enviar
-        </button>
+        <button>Enviar</button>
       </form>
-
-      <ul>
-        {
-          messages.map((message,i) => (
-            <li key={i}>
-              {message}
-            </li>
-          ))
-        }
-      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
