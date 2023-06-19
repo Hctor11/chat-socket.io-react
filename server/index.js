a058d4bcbd12533af8e88fc34d31a2a0002580d1
hector <hectorrrivera1010@gmail.com>
2023-06-18 22:20:43 -0600

    added connection between the server and the client

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 import express from 'express'
 import http from 'http'
-import { Server } from 'socket.io';
+import { Server as SocketServer} from 'socket.io';
 
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server)
+const io = new SocketServer(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'HEAD', 'POTS']
+    }
+})
+
+io.on('connection', socket => {
+    console.log('cliente conectado');
+})
 
 server.listen(3000)
 console.log('Server on port ', 3000);
